90c937531af11b14a02fdbe5876fc10f116ee30b
Ilya Margolin <ilya@jimdo.com>
2015-09-04 12:18:32 +0200

    Initial

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM nginx
+
+COPY jessie-backports.preferences /etc/apt/preferences.d/
+RUN echo "deb http://http.debian.net/debian jessie-backports main" >> /etc/apt/sources.list
+RUN apt-get update
+RUN apt-get install -y vim fortune-mod
+RUN apt-get install -y python3
+RUN apt-get install -y python3-aiohttp
+RUN apt-get install -y python3-pip
+RUN pip3 install graypy
+
+COPY nginx.conf /etc/nginx/nginx.conf
+RUN mkdir /var/www
+RUN /usr/games/fortune > /var/www/index.html
+#ENTRYPOINT /usr/sbin/nginx
+
+COPY httpserver.py /
+ENTRYPOINT python3 /httpserver.py
---
+++ httpserver.py
@@ -0,0 +1,50 @@
+import asyncio
+from aiohttp import web
+import logging
+import graypy
+
+
+def build_small_png():
+    import struct
+    import zlib
+    def chunk(type, data):
+        return (struct.pack('>I', len(data)) + type + data
+                + struct.pack('>I', zlib.crc32(type + data)))
+    png = (b'\x89PNG\r\n\x1A\n'
+           + chunk(b'IHDR', struct.pack('>IIBBBBB', 1, 1, 1, 0, 0, 0, 0))
+           + chunk(b'IDAT', zlib.compress(struct.pack('>BB', 0, 0)))
+           + chunk(b'IEND', b''))
+    return png
+
+
+PNG = build_small_png()
+GRAYLOG = graypy.GELFHandler('172.17.42.1', debugging_fields=False)
+
+
+@asyncio.coroutine
+def handle(request):
+    imgfile = request.match_info.get('imgfile', 'blank.png')
+    record = logging.LogRecord('frontend', 6, '', 0, 'frontend.metrics', None, None)
+    record.http_host = request.headers['host']
+    record.query_string = request.query_string
+    GRAYLOG.emit(record)
+
+    return web.Response(body=PNG)
+
+
+@asyncio.coroutine
+def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/{imgfile}', handle)
+
+    srv = yield from loop.create_server(app.make_handler(),
+                                        '0.0.0.0', 8080)
+    print("Server started at http://0.0.0.0:8080")
+    return srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
